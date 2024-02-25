0d3aaed1396fa88d0f1d1dccd2f2305bd729f562
Ted Loungou <loungouviny15@gmail.com>
2024-02-25 18:15:46 +0100

    update url import for django new version

--- porfolio_project/urls.py
+++ porfolio_project/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import include, url
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib import admin
 
+from django.urls import re_path, include
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('porfolio.urls'))
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', include('porfolio.urls'))
 ]
+
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
