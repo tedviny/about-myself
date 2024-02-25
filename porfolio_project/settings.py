2185a7ba6e33456a19fd45a176d853e9375d039a
Ted Loungou <loungouviny15@gmail.com>
2024-02-25 17:59:08 +0100

    remove calling of django-heroku

--- porfolio_project/settings.py
+++ porfolio_project/settings.py
@@ -137,5 +137,3 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
