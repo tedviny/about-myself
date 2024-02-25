63d964b1dcc63d1c3aaa4a8550abe974447efb25
Ted Loungou <loungouviny15@gmail.com>
2024-02-25 18:06:25 +0100

    update url import

--- porfolio/urls.py
+++ porfolio/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import include, url
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    re_path(r'^$', home, name='index'),
 ]
